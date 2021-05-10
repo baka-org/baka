a9143761a5899ab2012c7334d3335b8ddcfb1d1b
Helloyunho <yunho050840@gmail.com>
2021-05-11 01:26:17 +0900

    :sparkles: Make a new parser

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use baka::setting::read_plugin;
 use baka::{
+    parser::parse_args,
     setting::{read_project_setting, read_root_setting},
 };
+use std::env::args;
 
 fn main() {
     // TEST
-    read_root_setting();
-    read_project_setting();
-    read_plugin();
+    // read_root_setting();
+    // read_project_setting();
+    // read_plugin();
 
+    let args = args();
+    let args_parsed = parse_args(args);
+    println!(
+        "Flags: {:?} Subcommand: {} Args: {:?}",
+        args_parsed.baka_flags, args_parsed.subcommand, args_parsed.args
+    );
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,52 @@
-pub struct Parser {
-   pub args: Vec<String>,
-   pub sub_commands: Vec<SubCommand>,
+// pub struct Parser {
+//    pub args: Vec<String>,
+//    pub sub_commands: Vec<SubCommand>,
+// }
+
+// pub struct SubCommand {
+//     pub name: String,
+//     pub help: String,
+//     pub description: String,
+// }
+
+use std::env::Args;
+
+pub struct BakaArgs {
+  pub baka_flags: Vec<String>,
+  pub subcommand: String,
+  pub args: Vec<String>,
 }
 
-pub struct SubCommand {
-    pub name: String,
-    pub help: String,
-    pub description: String,
+pub fn parse_args(args: Args) -> BakaArgs {
+  let mut baka_flags: Vec<String> = vec![];
+  let mut subcommand = String::from("");
+  let mut argss: Vec<String> = vec![];
+  let mut parsing_finished = 0;
+  let mut get_flag_value = false;
+  for (i, arg) in args.enumerate() {
+    if i == 0 {
+      continue;
+    }
+    if parsing_finished == 2 {
+      argss.push(arg);
+    } else if arg.starts_with("-") {
+      get_flag_value = true;
+      baka_flags.push(arg);
+    } else {
+      if get_flag_value {
+        baka_flags.push(arg);
+        get_flag_value = false
+      } else if parsing_finished == 0 {
+        parsing_finished += 1;
+        subcommand = arg;
+        parsing_finished += 1;
+      }
+    }
+  }
+
+  BakaArgs {
+    baka_flags: baka_flags,
+    subcommand: subcommand,
+    args: argss,
+  }
 }
