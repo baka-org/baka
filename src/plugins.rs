2e2efe3e9f704f70f1e0cbacd2664964c36319e2
Akiacode <catry.me@gmail.com>
2021-05-15 20:14:15 +0900

    Add plugins

--- src/plugins.rs
+++ src/plugins.rs
@@ -1,22 +1,28 @@
 use serde::{Deserialize, Serialize};
-use std::collections::BTreeMap;
+use std::{
+    collections::BTreeMap,
+    env,
+    fs::{self},
+    io::Read,
+    path::PathBuf,
+};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-pub struct Plugin {
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
+pub struct PluginSetting {
     pub name: String,
     pub version: String,
     pub cmd: BTreeMap<String, PluginCmd>,
     pub path: PluginPath,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 pub struct PluginCmd {
-    pub exec: Option<String>,
+    pub exec: String,
     pub description: Option<String>,
     pub help: Option<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct PluginPath {
     // If all is null, should have at least one of darwin, win, Linux, other
     pub all: Option<String>,
@@ -24,4 +30,57 @@ pub struct PluginPath {
     pub win: Option<String>,
     pub linux: Option<String>,
     pub other: Option<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Plugin {
+    pub name: String,
+    pub version: String,
+    pub path: PathBuf,
+    pub settings: PluginSetting,
+}
+
+pub fn plugins() -> Vec<Plugin> {
+    let mut plugins = Vec::new();
+
+    let path = env::var("baka_plugins").unwrap();
+    let read_dir = fs::read_dir(path).unwrap();
+
+    for dir in read_dir.flatten() {
+        let dir_plugins = fs::read_dir(dir.path()).unwrap();
+
+        for dir_plugin in dir_plugins.flatten() {
+            let path = dir.path();
+            let check_files = vec!["plugin.json", "plugin.toml", "plugin.yaml"];
+
+            if check_files.contains(&dir_plugin.file_name().to_str().unwrap()) {
+                let mut file = fs::File::open(dir_plugin.path()).unwrap();
+                let mut buf = String::new();
+                file.read_to_string(&mut buf).unwrap();
+
+                let serde = {
+                    if dir_plugin.file_name() == "plugin.json" {
+                        serde_json::from_str::<PluginSetting>(buf.as_str()).unwrap()
+                    } else if dir_plugin.file_name() == "plugin.toml" {
+                        toml::from_str::<PluginSetting>(buf.as_str()).unwrap()
+                    } else if dir_plugin.file_name() == "plugin.yaml" {
+                        serde_yaml::from_str::<PluginSetting>(buf.as_str()).unwrap()
+                    } else {
+                        panic!("Error: plugin setting file (serde)");
+                    }
+                };
+
+                plugins.push(Plugin {
+                    name: serde.name.clone(),
+                    version: serde.version.clone(),
+                    path,
+                    settings: serde,
+                })
+            } else {
+                panic!("Not Found: `.plugin.[json, toml, yaml]`");
+            }
+        }
+    }
+
+    plugins
 }
