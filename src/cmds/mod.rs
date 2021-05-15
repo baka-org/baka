21e60c34bc3d7bf49729230b9c6165873d5c0bf2
Akiacode <catry.me@gmail.com>
2021-05-15 20:13:34 +0900

    modified

--- src/cmds/mod.rs
+++ src/cmds/mod.rs
@@ -7,6 +7,7 @@ pub fn match_subcommands(baka: BakaArgs) {}
 fn command_output(pacakge_name: &str, subcommand: &str, args: Vec<String>) -> Child {
     Command::new(pacakge_name)
         .arg(subcommand)
+        .args(args)
         .spawn()
-        .expect(&format!("{} command failed to start", pacakge_name))
+        .unwrap_or_else(|_| panic!("{} command failed to start", pacakge_name))
 }
